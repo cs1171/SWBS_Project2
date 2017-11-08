37d965168c29fe46ee174aba6d50861b23e13c70
cs1171 <cdsoto2@miners.utep.edu>
2017-11-07 17:00:27 -0700

    adding another php file to handle logging out

--- mainpage.php
+++ mainpage.php
@@ -16,23 +16,28 @@ include('signin.php');
     </style>
 </head>
 <body>
-<form action="mainpage.php">
-    <div class="container">
-        <label><b>Username:</b></label>
-        <input type="text" name="un" required>
-        <br>
-        <label><b>Password: </b></label>
-        <input type="text" name="pw" required>
+<?php // this block should only show if user is not signed in
+if(!(isset($_SESSION['login']))) { ?>
+    <form action="" method="post">
+        <div class="container">
+           <label><b>Username:</b></label>
+            <input id="un" type="text" name="un" placeholder="username" required>
+            <br>
+           <label><b>Password: </b></label>
+           <input id="pw" type="password" name="pw" placeholder="password" required>
 
-        <button type="submit">Sign in</button>
-    </div>
-</form><br><br>
+           <button type="submit" name="submit" value = 'login'>Sign in</button>
+     </div>
+    </form>
+<?php } ?>
+<br><br>
 This is the main page. Just imagine that there is interesting stuff here.<br>
     You can sign in if you want to, or click one of the links.
 <br><br>
-<?php if(isset($_SESSION['login'])) { ?>
-    <a href="mainpage.php"><u>Sign out</u></a>
-<?php } session_destroy() ?>
+<?php // should only show sign out link if user is signed in
+if(isset($_SESSION['login'])) { ?>
+    <a href="logout.php"><u>Sign out</u></a>
+<?php } ?>
     <a href="admin.php"><u>Admin page</u></a>
     <a href="user.php"><u>User page</u></a>
 </body>
